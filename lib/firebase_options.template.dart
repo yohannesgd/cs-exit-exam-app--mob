15dcd1c7e1b8f2d894802bb433b36061605cb6b2
gurmuyohannes3-sketch <gurmuyohannes3@gmail.com>
2026-06-22 14:29:52 +0300

    Added .gitignore and template for firebase_options

---
+++ lib/firebase_options.template.dart
@@ -0,0 +1,8 @@
+class FirebaseOptions {
+  static const String apiKey = "YOUR_API_KEY_HERE";
+  static const String appId = "YOUR_APP_ID_HERE";
+  static const String messagingSenderId = "YOUR_SENDER_ID_HERE";
+  static const String projectId = "YOUR_PROJECT_ID_HERE";
+  static const String databaseURL = "YOUR_DATABASE_URL_HERE";
+  static const String storageBucket = "YOUR_STORAGE_BUCKET_HERE";
+}
